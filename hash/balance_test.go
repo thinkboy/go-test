ba9d6e9f8270f2ed97dc38bc2e6be889cfe938fa
liudingjoey <liudingjoey@didichuxing.com>
2018-02-05 17:40:10 +0800

    update

--- hash/balance_test.go
+++ hash/balance_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestHash(t *testing.T) {
-	coun := make(map[int]int)
+	coun := make(map[uint32]int)
 	testMap := make(map[uint32]int)
 	for i := 0; i < 10000000; i++ {
 		str := fmt.Sprintf("test-%09d", i)
@@ -16,21 +16,7 @@ func TestHash(t *testing.T) {
 		testMap[v/65536]++
 	}
 	for n, c := range testMap {
-		if n >= 0 && n < 10000 {
-			coun[10000] += c
-		} else if n >= 10000 && n < 20000 {
-			coun[20000] += c
-		} else if n >= 20000 && n < 30000 {
-			coun[30000] += c
-		} else if n >= 30000 && n < 40000 {
-			coun[40000] += c
-		} else if n >= 40000 && n < 50000 {
-			coun[50000] += c
-		} else if n >= 50000 && n < 60000 {
-			coun[60000] += c
-		} else if n >= 60000 && n < 70000 {
-			coun[70000] += c
-		}
+		coun[n/10000] += c
 	}
 	for n, c := range coun {
 		fmt.Println(n, c)
